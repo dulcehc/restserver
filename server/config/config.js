a4936171d700910c753218350a693d444094652d
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-10-15 22:31:19 -0500

    environment variable

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb://user-cafe:123456M@ds037468.mlab.com:37468/cafe_test';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
